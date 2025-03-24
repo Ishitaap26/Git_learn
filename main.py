2d21aa3486f5a4e0c991f060d4ad13b235a820f9
tecknowcode <siddheshsir@gmail.com>
2025-03-24 19:44:40 +0530

    added subtraction feature

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ class calculator:
     def add (self, a, b):
         return a + b
     
+    def subtract (self, a, b):
+        return a - b
+    
 if __name__ == "__main__":
 
     calc = calculator()
     print("Addition: ", calc.add(5,9))
+    print("Subtraction: ", calc.subtract(8,3))
